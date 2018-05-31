267f8c315234ad9c292bc835a4f322bb9b6f11df
benedetto pellerito <benedettopellerito@gmail.com>
2018-05-31 04:14:50 +0200

    media clean fix rela

---
+++ app/src/main/java/info/pelleritoudacity/android/rcapstone/media/MediaEvent.java
@@ -0,0 +1,112 @@
+package info.pelleritoudacity.android.rcapstone.media;
+
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+
+import info.pelleritoudacity.android.rcapstone.R;
+import info.pelleritoudacity.android.rcapstone.utility.Costants;
+
+public class MediaEvent implements Player.EventListener {
+
+    private TextView mTvErrorPlayer;
+    private Handler mHandler;
+    private MediaPlayer mListener;
+    private MediaSession mMediaSession;
+    private TextView mTvCountDown;
+    Runnable mRunnableRemainingPlay;
+
+    public MediaEvent(MediaPlayer listener, MediaSession mediaSession,Handler handler,Runnable runnable, TextView tvCountDown, TextView tvErrorPlayer) {
+        mListener = listener;
+        mTvCountDown = tvCountDown;
+        mTvErrorPlayer = tvErrorPlayer;
+        mMediaSession = mediaSession;
+        mHandler = handler;
+        mRunnableRemainingPlay =  runnable;
+    }
+
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray
+            trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+        switch (playbackState) {
+
+            case Player.STATE_BUFFERING:
+                mListener.visibilityProgressBar(true);
+
+                break;
+
+            case Player.STATE_READY:
+                mListener.showPlayer();
+                if (mHandler != null) {
+                    mHandler.post(mRunnableRemainingPlay);
+                }
+                break;
+            default:
+        }
+
+        if (Costants.IS_MEDIA_SESSION) {
+            if (mMediaSession != null) {
+                mMediaSession.mediaSessionState(playWhenReady, playbackState);
+
+            }
+        }
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+        mTvErrorPlayer.setText(R.string.error_video);
+        mTvErrorPlayer.setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
+    }
+
+}
