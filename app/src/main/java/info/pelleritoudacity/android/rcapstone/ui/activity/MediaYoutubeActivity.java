e951307583b69a61b4de870521de37270e485759
benedetto pellerito <benedettopellerito@gmail.com>
2018-06-12 06:11:29 +0200

    add youtube player

---
+++ app/src/main/java/info/pelleritoudacity/android/rcapstone/ui/activity/MediaYoutubeActivity.java
@@ -0,0 +1,60 @@
+package info.pelleritoudacity.android.rcapstone.ui.activity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+
+import info.pelleritoudacity.android.rcapstone.R;
+import info.pelleritoudacity.android.rcapstone.utility.ActivityUI;
+import info.pelleritoudacity.android.rcapstone.utility.Costants;
+
+public class MediaYoutubeActivity extends YouTubeFailureRecoveryActivity {
+
+    private String mYoutubeStr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityUI.youtubeFullScreen(this);
+        setContentView(R.layout.activity_media_youtube);
+
+        YouTubePlayerFragment youTubePlayerFragment =
+                (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+        youTubePlayerFragment.initialize(Costants.YOUTUBE_DEVELOPER_KEY, this);
+
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            mYoutubeStr = intent.getStringExtra(Costants.EXTRA_YOUTUBE_PARAM);
+            String searchQueryYoutube = "?v=";
+            mYoutubeStr = mYoutubeStr.substring(mYoutubeStr.indexOf(searchQueryYoutube) + searchQueryYoutube.length());
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            if (!TextUtils.isEmpty(mYoutubeStr))
+                if(Costants.YOUTUBE_CLIENT_AUTOSTART){
+                    player.loadVideo(mYoutubeStr);
+
+                }else {
+                    player.cueVideo(mYoutubeStr);
+
+                }
+        }
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
+    }
+
+}
+
+
